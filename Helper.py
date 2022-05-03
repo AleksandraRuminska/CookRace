e410e3da9dadcbf3add9244e98c0a96e8f6254c1
Aleksandra <alexrumi2000@gmail.com>
2022-05-03 11:29:51 +0200

    Add helpers' sprites and helper class draft

---
+++ Helper.py
@@ -0,0 +1,15 @@
+from Tile import Tile
+from pathfinding.finder.a_star import AStarFinder
+SPRITE_SIZE = 50
+
+
+class Helper(Tile):
+    def __init__(self, image_name, col, row_count):
+        super().__init__(image_name, col, row_count)
+
+    def find_path(self, grid, index_x, index_y):
+        start = grid.node(self.rect.x/SPRITE_SIZE, self.rect.y/SPRITE_SIZE)
+        end = grid.node(index_x/SPRITE_SIZE, index_y/SPRITE_SIZE)
+
+        finder = AStarFinder()
+        path = finder.find_path(start, end, grid)
