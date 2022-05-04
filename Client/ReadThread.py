214d0053589eb0b5037c934de176e299a7524f83
Aleksandra <alexrumi2000@gmail.com>
2022-05-04 13:40:58 +0200

    Add ability to move more than one plate

--- Client/ReadThread.py
+++ Client/ReadThread.py
@@ -5,11 +5,11 @@ from Messages.MessageType import MessageType
 SPRITE_SIZE = 50
 
 class ReadThread(threading.Thread):
-    def __init__(self, client, cooks, plate, semaphore):
+    def __init__(self, client, cooks, movables, semaphore):
         threading.Thread.__init__(self)
         self.client = client
         self.cooks = cooks
-        self.plate = plate
+        self.movables = movables
         self.semaphore = semaphore
 
     def run(self):
@@ -49,13 +49,19 @@ class ReadThread(threading.Thread):
                 if self.cooks[in_data[1]].is_carrying():
                     self.cooks[in_data[1]].put_down()
                 else:
-                    self.cooks[in_data[1]].pick_up(self.plate)
+                    for obj in self.movables:
+                        if self.cooks[in_data[1]].rect.y - SPRITE_SIZE / 2 <= obj.rect.y <= \
+                                self.cooks[in_data[1]].rect.y + SPRITE_SIZE / 2 :
+                            if self.cooks[in_data[1]].rect.x - SPRITE_SIZE <= obj.rect.x \
+                                    <= self.cooks[in_data[1]].rect.x + SPRITE_SIZE:
+                                self.cooks[in_data[1]].pick_up(obj)
+
+                    #self.cooks[in_data[1]].pick_up(self.plate)
 
             elif in_data[0] == MessageType.PUTINPLACE:
                 self.cooks[in_data[1]].rect.x = in_data[2]*SPRITE_SIZE + in_data[3]
                 self.cooks[in_data[1]].rect.y = in_data[4]*SPRITE_SIZE + in_data[5]
                 self.cook.collision = False
-
 
             print("From Server :", in_data.decode())
             if 'bye' == in_data.decode():
